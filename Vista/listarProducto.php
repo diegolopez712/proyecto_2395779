b5ae0c083d324359eda755042b658bc0c80dc7e5
“DiegoLopez” <diegol720@gmail.com>
2022-05-17 20:13:09 -0500

    Commit 2: Actualización de la vista listarProducto.php

--- Vista/listarProducto.php
+++ Vista/listarProducto.php
@@ -7,7 +7,7 @@ require_once('../Controlador/controladorProducto.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista Producto</title>
+    <title>Lista de  Productos</title>
 </head>
 <body>
     <a href="../Controlador/controladorProducto.php?vista=registrarProducto.php" >Registrar</a>
