c025a2f953b2ace33c19839199452b6a6b133523
“DiegoLopez” <diegol720@gmail.com>
2022-05-27 18:46:33 -0500

    Commit crud maestro-detalle 27-05-2022

--- crud_2395779 (1).sql
+++ crud_2395779 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-05-2022 a las 02:03:12
+-- Tiempo de generación: 28-05-2022 a las 01:45:15
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -41,6 +41,8 @@ INSERT INTO `categoria` (`idCategoria`, `nombre`) VALUES
 (14, 'COMPUTADOR12'),
 (1, 'FRUTAS'),
 (5, 'LICORES'),
+(20, 'MOUSE'),
+(19, 'portatil'),
 (16, 'VENTANAS');
 
 -- --------------------------------------------------------
@@ -68,6 +70,48 @@ INSERT INTO `producto` (`idProducto`, `idCategoria`, `nombre`, `precio`, `estado
 (5, 17, 'SAMSUNG', 78888.00, b'1'),
 (7, 5, 'VINO', 450000.00, b'1');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idRol` int(1) NOT NULL,
+  `nombre` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idRol`, `nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Asesor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(5) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `contrasena` varchar(200) NOT NULL,
+  `idRol` int(1) NOT NULL,
+  `estado` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `email`, `contrasena`, `idRol`, `estado`) VALUES
+(1, 'diego@gmail.com', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 1, 1),
+(2, 'prueba@gmail.com', '1234', 1, 0),
+(3, 'prueba@gmail.com', '1234', 1, 0);
+
 --
 -- Índices para tablas volcadas
 --
@@ -87,6 +131,19 @@ ALTER TABLE `producto`
   ADD KEY `idCategoria` (`idCategoria`);
 
 --
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idRol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idRol` (`idRol`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -94,13 +151,25 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
   MODIFY `idProducto` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idRol` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -111,6 +180,12 @@ ALTER TABLE `producto`
 --
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idRol`) REFERENCES `rol` (`idRol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
